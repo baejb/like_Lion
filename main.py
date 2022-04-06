2eb20fc2e6a9de12f669c33697969aba4d0b6e63
Baejb <bjb6478@naver.com>
2022-04-06 18:46:48 +0900

    코드라이언 데이터상자만들기

---
+++ main.py
@@ -0,0 +1,6 @@
+import random
+import time
+    #for x in range(30): 0부터 29까지 x 라는 이름으로 저장을 하겠다 .
+lunch = random.choice(["된장찌개" , "피자" ,"제육볶음","치킨", "떡볶이"])
+dinner = random.choice(["김밥","쫄면","돈까스"])
+print(lunch)
