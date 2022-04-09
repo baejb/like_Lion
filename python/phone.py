513c4cb9d2e421af0bfae6b9a8528ac464f9a740
Baejb <bjb6478@naver.com>
2022-04-10 02:31:27 +0900

    2차 과제 완성본

--- python/phone.py
+++ python/phone.py
@@ -1,6 +1,4 @@
-total_question = {}
-info = {}
-
+total_list = []
 while True:
     print("-----------------멋쟁이 사자처럼 전화번호부 -----------------")
     print("-----------1) 추가 2) 조회 3) 수정 4) 삭제 q) 종료---------- ")
@@ -13,31 +11,34 @@ while True:
         phone_number = input(name + "님의 번호를 입력해주세요 :")
         mail = input(name + "님의 메일을 입력해주세요 :")
         information = {'이름': name, '전화번호': phone_number, '메일': mail}
-        info[name] = information
+        total_list.append(information)
     elif choice_menu == '2':
         name = input("조회를 원하는 이름은 입력해주세요 :")
-        if info[name] is None:
-            print("찾을 수 없습니다. ")
-        else:
-            print(info[name])
+        for i in total_list:
+            if name == i['이름']:
+                print(i)
+            else:
+                print("등록되지 않은 정보입니다.")
     elif choice_menu == '3':
         name = input("수정을 원하는 이름을 입력해주세요 : ")
-        if info[name] is None:
-            print("찾을 수 없습니다. ")
-        else:
-            key,value =input("수정을 원하는 항목과 내용을 입력해주세요").split()
-            if key == "이름":
-                info[name].update({'이름':value})
-            elif key == "전화번호":
-                info[name].update({'전화번호':value})
-            elif key == "메일":
-                info[name].update({'메일':value})
+        for i in total_list:
+            if name == i['이름']:
+                key,value = ("수정을 원하는 항목과 내용을 입력해주세요 :").split()
+
+                if(key == "전화번호"):
+                    i['전화번호']=value
+                elif(key == "메일"):
+                    i['메일']=value
+            else:
+                print("등록되지 않은 정보입니다.")
+
     elif choice_menu =='4':
         name = input("삭제를 원하는 이름을 입력해주세요 :")
-        if info[name] is None:
-            print("찾을 수 없습니다.")
-        else:
-            del info[name]
+        for i in total_list:
+           if name == i['이름']:
+            del i['이름']
+            del i['전화번호']
+            del i['메일']
 
     elif choice_menu == "q":
         break
